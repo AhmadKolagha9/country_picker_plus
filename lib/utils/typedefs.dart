8b6c80ae29501afd130ce5edd970110555501685
ahmad_agha <ahmadkolagha99@gmail.com>
2025-12-03 22:37:20 +0300

    first commit

---
+++ lib/utils/typedefs.dart
@@ -0,0 +1,14 @@
+import 'package:country_picker_plus/country.dart';
+import 'package:flutter/material.dart';
+
+/// Returns true when a country should be included in lists / dialogs
+/// offered to the user.
+typedef ItemFilter = bool Function(Country country);
+
+///Predicate to be satisfied in order to add country to search list
+typedef SearchFilter = bool Function(Country country, String searchWord);
+
+typedef ItemBuilder = Widget Function(Country country);
+
+/// Simple closure which always returns true.
+bool acceptAllCountries(_) => true;
